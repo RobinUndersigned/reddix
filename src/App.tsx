283032c1a20356e470d1db216b773a40d20da918
decker <robin.decker@neofonie.de>
2022-02-15 14:22:48 +0100

    tailwind and linting

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="h-full w-full">
+      <header className="fixed flex left-0 right-0 top-0 z-index-50 items-center bg-">
+        Logo
       </header>
     </div>
   );
