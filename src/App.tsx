2b99550dd1f0fecb6efd61efae07112cfe3891de
decker <robin.decker@neofonie.de>
2022-02-15 14:53:34 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, {Fragment} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import {BellIcon, MenuIcon, XIcon} from "@heroicons/react/solid";
