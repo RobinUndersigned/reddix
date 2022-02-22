fdfc8c26c82e5c47cf6c6abe2e1bb018c4a7ad78
decker <robin.decker@neofonie.de>
2022-02-22 15:05:48 +0100

    updated seeds to users, subreddixs and posts

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,33 +1,76 @@
 import { Prisma } from '@prisma/client'
 import Db from "../src/db/PrismaClient"
 import { faker } from "@faker-js/faker";
+import {genSalt, hash} from "bcrypt";
 
 function generateRange(size: number): number[] {
   return [...Array.from(Array(size).keys())];
 }
 
-async function createUser()  {
+/**
+ * generate Users
+ */
+
+async function generateUser()  {
+  const salt = await genSalt(10)
   const seedUser: Prisma.UserCreateInput = {
     firstName: faker.name.firstName(),
     lastName: faker.name.lastName(),
     userName: `${faker.name.firstName()+faker.name.lastName()}`,
-    email: faker.name.firstName(),
-    password: "password"
+    email: faker.internet.email(),
+    password: await hash("password", salt)
   }
-  try {
-    const newUser = await Db.user.create({
-      data: seedUser,
-    })
-    console.log(`Created user with id: ${newUser.id}`)
-  } catch(err) {
-    console.log(err)
+  return seedUser;
+}
+
+/**
+ * Generate Subreddix
+ */
+function generateSubreddix()  {
+  const word = faker.lorem.word()
+  const seedSubreddix: Prisma.SubreddixCreateInput = {
+    name: word,
+    description: faker.lorem.sentence(),
+    url: word.toLowerCase(),
   }
+  return seedSubreddix;
+}
+
+function generatePost(seedUser: Prisma.UserCreateInput, seedSubreddix: Prisma.SubreddixCreateInput) {
+  const seedPost: Prisma.PostCreateInput = {
+    title: faker.lorem.sentence(),
+    content: faker.lorem.paragraphs(),
+    published: Math.random() > 0.5 ? true : false,
+    User: {
+      create: seedUser
+    },
+    Subreddix: {
+      create: seedSubreddix
+    }
+  }
+  return seedPost
 }
 
 async function main() {
   console.log(`Start seeding ...`)
-  generateRange(10).map((v) => {
-    createUser()
+  const posts = await Promise.all(
+    generateRange(10).map(async (v) => {
+      const user = await generateUser()
+      const subreddix = generateSubreddix()
+      const post = await generatePost(user, subreddix)
+      return post;
+    })
+  )
+
+  posts.map(async (post) => {
+    try {
+      const newPost = await Db.post.create({ data: post})
+      console.log("---------------------------")
+      console.log(`Created Post with id ${newPost.id} \n User with ID ${newPost.authorId} \n  and Subreddix with ID ${newPost.subreddixId}`)
+      console.log("---------------------------")
+    } catch(err) {
+      console.log(err)
+    }
   })
   console.log(`Seeding finished.`)
 }
